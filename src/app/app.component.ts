4cadce33e000a14a49f5bcf769054918568e5d75
王峰 <yangdejingjing@gmail.com>
2021-12-21 13:27:18 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+declare var luckysheet: any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'luckysheet';
+
+  ngAfterViewInit(): void {
+    //配置项
+    var options = {
+      container: 'luckysheet' //luckysheet为容器id
+    }
+    luckysheet.create(options)
+  }
+
 }
